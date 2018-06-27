65230d9172a98999764c41f7a862492eab6e75ba
Anindya Chakraborty <anindya100c@gmail.com>
2018-06-28 03:19:11 +0530

    retrive

---
+++ bin/WebContent/js/user/Properties.js
@@ -0,0 +1,15 @@
+var config={
+		"protocol" : "http",
+		"port" : "8080",
+		"domain" : "localhost",
+		"project" : "TradeWork"
+}
+
+/*
+ * Do not change the code below this point
+ * Only change the port number in the config object
+ */
+
+function getURI(){
+	return config.protocol +"://"+ config.domain +":"+ config.port +"/"+ config.project +"/api/" ;
+}
